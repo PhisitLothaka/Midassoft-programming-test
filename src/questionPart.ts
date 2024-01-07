f4b479eb93630e52d08387e77c1a1d822344c432
PhisitLothaka <tenphisit1998@gmail.com>
2024-01-07 18:31:21 +0700

    v1 - unsuccess

--- src/questionPart.ts
+++ src/questionPart.ts
@@ -2,14 +2,20 @@ function getQuestionPart(arr: string[]): string[] {
   const phrases1: string = arr[0];
   const phrases2: string = arr[2];
   console.log(phrases1 + " , " + phrases2);
-  const objResult: { idx: number[]; var: string[] } = { idx: [], var: [] };
+  const objResult: { idxOld: number[]; var: string[] } = {
+    idxOld: [],
+    var: [],
+  };
   let result: string;
 
   for (let i = 0; i < phrases1.length; i++) {
     for (let n = 0; n < phrases2.length; n++) {
       if (phrases1[i] === phrases2[n]) {
         console.log(`${phrases1[i]},${i}===>${phrases2[n]},${n}`);
-        objResult?.idx.push(n - i);
+
+        objResult?.idxOld.push(n);
+        // if (n - objResult?.idxOld[objResult.idxOld.length - 1] === 1)
+        console.log(n, objResult?.idxOld[objResult.idxOld.length - 2]);
         objResult?.var.push(phrases1[i]);
       }
     }
@@ -20,11 +26,9 @@ function getQuestionPart(arr: string[]): string[] {
   //     console.log("getresut ", objResult.var[count]);
   //   }
   // }
-  let test = objResult.idx.filter(
-    (el, idx) => objResult.idx[idx] === objResult.idx[idx + 1]
-  );
+
   console.log(objResult);
-  console.log(test);
+
   return arr;
 }
 getQuestionPart(["BATHROOM", "BATH SALTS", "BLOODBATH"]);
