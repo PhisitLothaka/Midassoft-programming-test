7a407d7c84a52bdda6358af3348cb04b92eef845
PhisitLothaka <tenphisit1998@gmail.com>
2024-01-07 22:24:10 +0700

    questionPart did not pass

--- src/questionPart.ts
+++ src/questionPart.ts
@@ -1,34 +1,29 @@
 function getQuestionPart(arr: string[]): string[] {
-  const phrases1: string = arr[0];
-  const phrases2: string = arr[2];
-  console.log(phrases1 + " , " + phrases2);
-  const objResult: { idxOld: number[]; var: string[] } = {
-    idxOld: [],
-    var: [],
-  };
-  let result: string;
+  let varArr: string[] = [];
 
-  for (let i = 0; i < phrases1.length; i++) {
-    for (let n = 0; n < phrases2.length; n++) {
-      if (phrases1[i] === phrases2[n]) {
-        console.log(`${phrases1[i]},${i}===>${phrases2[n]},${n}`);
+  let result: string = "";
+  let longStr: string;
+  let shortStr: string;
 
-        objResult?.idxOld.push(n);
-        // if (n - objResult?.idxOld[objResult.idxOld.length - 1] === 1)
-        console.log(n, objResult?.idxOld[objResult.idxOld.length - 2]);
-        objResult?.var.push(phrases1[i]);
+  arr[0].length > arr[1].length
+    ? ((longStr = arr[0]), (shortStr = arr[2]))
+    : ((shortStr = arr[0]), (longStr = arr[2]));
+
+  for (let i = 0; i < shortStr.length; i++) {
+    console.log("count ==>", i);
+    for (let n = 0; n < longStr.length; n++) {
+      if (shortStr[i] === longStr[n]) {
+        console.log(i, shortStr[i], "==>", n, longStr[n]);
+        if (shortStr[i + 1] === longStr[n + 1]) {
+          console.log(i, shortStr[i], "-->", n, longStr[n]);
+        }
       }
     }
   }
-  // for (let count = 0; count < objResult?.idx.length; count++) {
-  //   if (objResult.idx[count] === objResult.idx[count + 1]) {
-  //     // result += ;
-  //     console.log("getresut ", objResult.var[count]);
-  //   }
-  // }
 
-  console.log(objResult);
-
+  console.log("==>", varArr);
   return arr;
 }
+
 getQuestionPart(["BATHROOM", "BATH SALTS", "BLOODBATH"]);
+// getQuestionPart(["BEFRIEND", "GIRLFRIEND", "FRIENDSHIP"]);
